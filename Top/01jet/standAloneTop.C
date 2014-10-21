107cfb98d4ed28714c0adfd590c83abfaf68b9b2
Khakim <khakimjan@yahoo.com>
2014-10-21 23:15:54 +0200

    Top_01jet

---
+++ Top/01jet/standAloneTop.C
@@ -0,0 +1,48 @@
+#include "DY.C"
+#include "Top.C"
+
+
+TString directory = "rootfiles";
+
+
+//------------------------------------------------------------------------------
+//
+// standAloneTop
+//
+//------------------------------------------------------------------------------
+void standAloneTop(Int_t  njet          = 0,
+		   Bool_t useDataDriven = true,
+		   Int_t  printLevel    = 2)
+{
+  Double_t dyYield;
+  Double_t dyStatError;
+  Double_t dySystError;
+  Double_t dyScaleFactor;
+
+  DY(dyYield,
+     dyStatError,
+     dySystError,
+     dyScaleFactor,
+     njet,
+     "SF",
+     directory,
+     useDataDriven,
+     printLevel);
+
+  Double_t topYield;
+  Double_t topStatError;
+  Double_t topSystError;
+  Double_t topScaleFactor;
+
+  std::cout<<"dyScaleFactor: "<<dyScaleFactor<<std::endl;
+  Top(topYield,
+      topStatError,
+      topSystError,
+      topScaleFactor,
+      dyScaleFactor,
+      njet,
+      "All",
+      directory,
+      useDataDriven,
+      printLevel);
+}
